9f487e6728ca0f20de81857dc955c0dd3b4a8afe
Jiaory <sjz1005@gmail.com>
2021-05-05 11:58:57 -0700

    part 1 setup

---
+++ Lab3/main.js
@@ -0,0 +1,36 @@
+// specify a url, in this case our web server
+
+const url = "http://ec2-54-219-224-129.us-west-1.compute.amazonaws.com:2000/feed/random?q=weather"
+
+var tweetList = [];
+
+function fetchTweets() {
+    fetch(url)
+    .then(res => res.json()) .then(data => {  
+    // do something with data
+    console.log(data)
+    OUTER:
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < tweetList.length; j++) {
+            if (tweetList[j].includes(data.statuses[i].id)) {
+                console.log(data.statuses[i].id)
+                continue OUTER;
+            }
+        }
+        var tweetInfo = [];
+        tweetInfo.push(data.statuses[i].id);
+        tweetInfo.push(data.statuses[i].text);
+        tweetInfo.push(data.statuses[i].created_at);
+        tweetList.push(tweetInfo);
+    }
+    console.log(tweetList);
+    })
+    .catch(err => {
+        // error catching
+    console.log(err) }) 
+}
+
+function getTweets() {
+    fetchTweets();
+    var intervalID = setInterval(fetchTweets, 5000);
+}
